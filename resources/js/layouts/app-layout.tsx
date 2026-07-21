aa875a937ce3aaa06cd952ca4b277dedac745ce6
HowRiyo <howriyo@gmail.com>
2026-07-21 19:09:47 +0800

    perbaiki wrapper layout

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,22 +1,11 @@
-import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
-import type { BreadcrumbItem } from '@/types';
-
 export default function AppLayout({
-    breadcrumbs = [],
     children,
 }: {
-    breadcrumbs?: BreadcrumbItem[];
     children: React.ReactNode;
 }) {
     return (
-<<<<<<< HEAD
-        <AppLayoutTemplate breadcrumbs={breadcrumbs}>
-            {children}
-        </AppLayoutTemplate>
-=======
         <main>
             {children}
         </main>
->>>>>>> 6cfcb2d (tambah halaman login publik)
     );
 }
--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -1,5 +1,3 @@
-import AuthLayoutTemplate from '@/layouts/auth/auth-simple-layout';
-
 export default function AuthLayout({
     title = '',
     description = '',
@@ -9,14 +7,5 @@ export default function AuthLayout({
     description?: string;
     children: React.ReactNode;
 }) {
-<<<<<<< HEAD
-    return (
-        <AuthLayoutTemplate title={title} description={description}>
-            {children}
-        </AuthLayoutTemplate>
-    );
-}
-=======
     return <>{children}</>;
 }
->>>>>>> 6cfcb2d (tambah halaman login publik)
