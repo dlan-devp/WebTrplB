c1c8360e441c737c66db99c53a6c314f8190d10b
dlan-devp <mfadlan0913@gmail.com>
2026-07-21 08:07:28 +0800

    kirim data ke welcome react

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 //use Illuminate\Http\Request;
+use App\Models\Galeri;
+use App\Models\Jadwal;
 use App\Models\Mahasiswa;
+use App\Models\Testimoni;
 use Inertia\Inertia;
 
 class MahasiswaController extends Controller
@@ -12,6 +15,9 @@ class MahasiswaController extends Controller
 
         return Inertia::render('welcome', [
             'mahasiswa' => Mahasiswa::all(),
+            'testimoni' => Testimoni::all(),
+            'galeri' => Galeri::all(),
+            'jadwal' => Jadwal::all(),
             'test' => 'mantap'
         ]);
     }
