5aa72a902d70523043b5da63bfcba4fa5ea6a0b6
HowRiyo <howriyo@gmail.com>
2026-07-21 11:18:52 +0800

    update gaya scroll

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -11,12 +11,63 @@ import Navbar from '@/components/ui/Navbar';
 import Pengumuman from '@/components/ui/Pengumuman';
 import TestimoniKelas from '@/components/ui/TestimoniKelas';
 import type { Mahasiswa } from '@/types/AnggotaKelas.props';
+import { useEffect } from 'react';
 
 interface WelcomeProps {
   mahasiswa: Mahasiswa[];
 }
 
 export default function Welcome({ mahasiswa }: WelcomeProps) {
+  useEffect(() => {
+    let current = window.scrollY;
+    let target = current;
+    let rafId = 0;
+
+    const ease = 0.05;
+
+    const update = () => {
+      current += (target - current) * ease;
+
+      if (Math.abs(target - current) < 0.3) {
+        current = target;
+      }
+
+      window.scrollTo({
+          top: current,
+          behavior: "instant",
+      });
+
+      rafId = requestAnimationFrame(update);
+    };
+
+    const onWheel = (e: WheelEvent) => {
+      e.preventDefault();
+
+      const wheelMultiplier = 0.4;
+      
+      target += e.deltaY * wheelMultiplier;
+      
+      const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+
+
+      target = Math.max(
+          0,
+          Math.min(target + e.deltaY * wheelMultiplier, maxScroll)
+      );
+    };
+
+    rafId = requestAnimationFrame(update);
+
+    window.addEventListener("wheel", onWheel, {
+      passive: false,
+    });
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      window.removeEventListener("wheel", onWheel);
+    };
+  }, []);
+
   return (
     <>
       <Head title="Website TRPL-B" />
@@ -32,7 +83,9 @@ export default function Welcome({ mahasiswa }: WelcomeProps) {
         ))}
       </div>
 
-      <main className="relative z-10">
+      <main
+        className="relative z-10"
+      >
         <Navbar />
         <Hero />
         <JadwalKelas />
