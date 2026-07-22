c0af5533db46fb2bd7ef6101b9e1c5e721df10c1
HowRiyo <howriyo@gmail.com>
2026-07-22 10:01:29 +0800

    kesalahan berpikir pada midleware testimoni

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [MahasiswaController::class, 'index'])->name('home');
+Route::get('/testimoni', [TestimoniController::class, 'index'])->name('testimoni');
 
 Route::middleware('guest')->group(function () {
     Route::get('/public-auth', [PublicAuthController::class, 'index'])->name('auth');
-    Route::get('/testimoni', [TestimoniController::class, 'index'])->name('testimoni');
 });
 
 // Route::middleware(['auth', 'verified'])->group(function () {
