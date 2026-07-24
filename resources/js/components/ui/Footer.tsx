1321a43124d7111d997b5006599cb1a5fea46fcf
dlan-devp <mfadlan0913@gmail.com>
2026-07-24 21:40:10 +0800

    ubah teks footer

--- resources/js/components/ui/Footer.tsx
+++ resources/js/components/ui/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__inner">
-        <span>Kelas TI-3B &middot; dibuat oleh anak kelas, untuk anak kelas.</span>
+        <span>Kelas Trpl B &middot; dibuat oleh anak kelas, untuk anak kelas.</span>
         <span className="footer__year">2026/2027</span>
       </div>
     </footer>
